be7ffd50c49c0a900b0ff3d142623d8b21f98bcc
Avinash <avinash.devapp@gmail.com>
2025-08-22 19:48:44 +0530

    Text change

--- iOS-UI-Demo/ContentView.swift
+++ iOS-UI-Demo/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("iOS UI Demo")
+            Text("Avinash iOS UI Demo")
         }
         .padding()
     }
